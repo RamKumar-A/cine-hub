e94f618cd418c1e1b269aa6e8f69564bfaa1b7d9
Ram Kumar <ramk41934@gmail.com>
2024-11-22 21:03:25 +0530

    Updated Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
-import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
+import { motion, useMotionValueEvent } from 'framer-motion';
 import { useRef, useState } from 'react';
 import { HiOutlineMagnifyingGlass, HiOutlineTv } from 'react-icons/hi2';
 import { MdOutlineMovie } from 'react-icons/md';
 
 import { useLocation, useNavigate } from 'react-router-dom';
-
+const MotionPaper = motion(Paper);
 function NavBar({ scrollY }) {
   const [isHidden, setIsHidden] = useState(true);
   const lastRef = useRef(0);
@@ -17,7 +17,6 @@ function NavBar({ scrollY }) {
     const difference = y - lastRef.current;
     if (Math.abs(difference) > 50) {
       setIsHidden(difference < 50);
-
       lastRef.current = y;
     }
   });
@@ -29,50 +28,42 @@ function NavBar({ scrollY }) {
   }
 
   return (
-    <motion.div>
-      <Paper
-        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-        animate={isHidden ? 'visible' : 'hidden'}
-        variants={{
-          visible: {
-            y: '0%',
-          },
-          hidden: {
-            y: '100%',
-          },
-        }}
-        transition={{ type: 'tween', duration: 0.3 }}
-      >
-        <BottomNavigation showLabels value={currentLocation}>
-          <BottomNavigationAction
-            label="Movies"
-            value="/movies"
-            icon={<MdOutlineMovie size={20} />}
-            onClick={() => handleNavigation('/movies')}
-          />
-          <BottomNavigationAction
-            label="Shows"
-            value="/shows"
-            icon={<HiOutlineTv size={20} />}
-            onClick={() => handleNavigation('/shows')}
-          />
-          <BottomNavigationAction
-            label="Search"
-            value="/search"
-            icon={<HiOutlineMagnifyingGlass size={20} />}
-            onClick={() => handleNavigation('/search')}
-          />
-        </BottomNavigation>
-      </Paper>
-    </motion.div>
+    <MotionPaper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+      animate={isHidden ? 'visible' : 'hidden'}
+      variants={{
+        visible: {
+          y: '0%',
+        },
+        hidden: {
+          y: '100%',
+        },
+      }}
+      transition={{ type: 'tween', duration: 0.3 }}
+    >
+      <BottomNavigation showLabels value={currentLocation}>
+        <BottomNavigationAction
+          label="Movies"
+          value="/movies"
+          icon={<MdOutlineMovie size={20} />}
+          onClick={() => handleNavigation('/movies')}
+        />
+        <BottomNavigationAction
+          label="Shows"
+          value="/shows"
+          icon={<HiOutlineTv size={20} />}
+          onClick={() => handleNavigation('/shows')}
+        />
+        <BottomNavigationAction
+          label="Search"
+          value="/search"
+          icon={<HiOutlineMagnifyingGlass size={20} />}
+          onClick={() => handleNavigation('/search')}
+        />
+      </BottomNavigation>
+    </MotionPaper>
   );
 }
 
 export default NavBar;
-
-/* <BottomNavigationAction
-          label="Home"
-          icon={<HiHomeModern size={20} />}
-          onClick={() => handleNavigation('/')}
-        /> */
